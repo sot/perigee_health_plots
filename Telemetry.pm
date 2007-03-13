b112e05985198554dfd01b7f2f97b0e888210972
jeanconn <devnull@localhost>
2007-03-13 18:52:33 +0000

    *** empty log message ***

---
+++ Telemetry.pm
@@ -0,0 +1,345 @@
+package Telemetry::BinTable;
+
+use strict;
+use warnings;
+use Carp;
+use Class::MakeMethods::Standard::Hash( 
+					scalar => [ qw(
+						       file
+						       columns
+						       order
+						       header
+						       hdrtable
+						       bintable
+						       )
+						    ],
+					);
+use CFITSIO::Simple;
+
+sub new{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    my $arg_in = shift;
+    if ( defined $arg_in->{file} ){
+	$self->file($arg_in->{file});
+    }
+    if ( defined $arg_in->{columns}){
+	$self->columns($arg_in->{columns});
+    }
+    return $self;
+}
+
+sub length{
+    my $self = shift;
+    my $value = shift;
+    if (defined $value){
+	$self->{length} = $value;
+    }
+
+    if (defined $self->{length}){
+	return $self->{length};
+    }
+    else{
+	$self->process();
+	return $self->{length};
+    }
+
+}
+
+sub print{
+    my $self = shift;
+    $self->process();
+    my %table = %{$self->table()};
+    for my $keyword ( @{$self->order()} ){
+	my $data = $table{$keyword};
+	print $data->{order}, "\t";
+	print $keyword, "\t";
+	if (defined $data->{UNIT}){
+	    print $data->{UNIT};
+	}
+	print "\t";
+	if (defined $data->{FORM}){
+	    print $data->{FORM}
+	}
+	if (defined $data->{DIM}){
+	    print "(", $data->{DIM}, ")";
+	}
+	print "\t";
+	if ((defined $data->{LMIN}) and (defined $data->{LMAX})){
+	    print $data->{LMIN}, ":", $data->{LMAX};
+	}
+	print "\t";
+	if (defined $data->{comment}){
+	    print $data->{comment};
+	}
+	print "\n";
+    }
+}
+
+sub process{
+    my $self = shift;
+    unless (defined $self->file() ){
+	croak(__PACKAGE__ . "Telemetry file must be defined");
+    }
+    my $hdr = fits_read_hdr( $self->file, 2);
+    $self->header($hdr);
+    my @columns;
+    if (defined $self->columns()){
+	@columns = @{$self->{columns}};
+    }
+    my %fits = fits_read_bintbl( $self->file, @columns );
+    $self->bintable(\%fits);
+    $self->length( $fits{time}->nelem );
+    my @order;
+    my %data;
+    for my $i (0 ... 50){
+	my $keyword = $hdr->{"TTYPE$i"};
+	if (defined $keyword){
+	    # strip off trailing spaces
+	    $keyword =~ s/\s+$//;
+	    #lowercase everything
+	    $keyword =~ tr/A-Z/a-z/;
+	    push @order, $keyword;
+	    my %keyvals;
+	    $keyvals{order} = $i;
+#	    print Dumper $hdr->{"COMMENTS"};
+	    if (defined $hdr->{"COMMENTS"}->{"TTYPE$i"}){
+		my $comment = $hdr->{"COMMENTS"}->{"TTYPE$i"};
+		$comment =~ s/\s+$//;
+		$keyvals{comment} = $comment;
+	    }
+	    for my $hdrkey qw( UNIT LMAX LMIN FORM DIM ){
+		if (defined $hdr->{"T${hdrkey}${i}"}){
+		    my $infokey =  $hdr->{"T${hdrkey}${i}"};
+		    #strip off trailing spaces
+		    $infokey =~ s/\s+$//;
+		    #lowercase everything
+		    my $lowerhdrkey = $hdrkey;
+		    $lowerhdrkey =~ tr/A-Z/a-z/;
+		    $keyvals{$lowerhdrkey} = $infokey;
+		}
+	    }
+
+	    $data{$keyword} = \%keyvals;	    
+	}
+	
+    }
+    $self->hdrtable(\%data);
+    $self->order(\@order);
+}
+
+1;
+
+
+package Telemetry::BinTable::ACA0;
+
+use strict;
+use warnings;
+
+use base 'Telemetry::BinTable';
+
+use PDL;
+use PDL::NiceSlice;
+
+our @ISA =  qw( Telemetry::BinTable );
+
+
+sub process{
+    my $self = shift;
+    $self->Telemetry::BinTable::process();
+    # Convert any image data to 8x8 PDL
+    my $imgraw = $self->bintable->{imgraw};
+    my $templateraw = zeroes( 8, 8, $self->length);
+    my $dim = $imgraw->getdim(0);
+    $templateraw(0:($dim-1), 0:($dim-1), :) .= $imgraw;
+    $self->bintable->{imgraw} = $templateraw;
+    # Add any columns that would only exist in the 8x8 data
+    my $template = Telemetry::BinTable->new({ file => '/proj/gads6/jeanproj/perigee_health_plots/aca8x8.fits.gz'});
+    $template->process();
+    for my $key ( keys %{$template->bintable} ){
+	next if ( defined $self->bintable->{$key} );
+	my $tempdata = zeroes( $self->length );
+	$self->bintable->{$key} = $tempdata;
+    }
+    $self->bintable->{imgdim} = zeroes( $self->length ) + $dim;
+    my @columns = keys %{$template->bintable};
+    $self->columns(\@columns);
+
+}    
+
+
+1;
+
+
+
+
+
+package Telemetry::Interval;
+
+use base 'Telemetry::BinTable';
+
+use strict;
+use warnings;
+use Carp;
+use PDL;
+
+use Class::MakeMethods::Standard::Hash (
+					scalar => [ qw(
+						       file_list
+						       telem
+						       telemtype
+						       columns
+						       )
+						    ],
+					);
+						       
+
+sub new{
+
+    my $class = shift;
+    my $self = {};
+
+    bless $self, $class;
+    my $arg_in = shift;
+
+    if (defined $arg_in->{file_list}){
+	$self->file_list($arg_in->{file_list});
+    }
+    if (defined $arg_in->{telemtype}){
+	$self->telemtype($arg_in->{telemtype});
+    }
+    if (defined $arg_in->{columns}){
+	$self->columns($arg_in->{columns});
+    }
+
+    return $self;
+}
+
+
+
+sub combine_telem{
+
+    my $self = shift;
+
+    unless ( defined $self->file_list()){
+	croak(__PACKAGE__ . "No telemetry files specified.");
+    }	     
+
+    unless ( ( defined $self->telemtype() ) or ( defined $self->columns() ) ){
+	croak(__PACKAGE__ . "Must specify type of telemetry or list of columns.");
+    }
+
+    my $file_list = $self->file_list();
+
+    my %telem;
+
+    
+    for my $file (@{$self->file_list()}){
+	my $telem_chunk; 
+	if (defined $self->telemtype()){
+	    if ($self->telemtype() eq 'aca0'){
+		$telem_chunk = Telemetry::BinTable::ACA0->new({ file => $file, columns => $self->columns() });
+		$telem_chunk->process();
+	    }
+	}
+	else{
+	    $telem_chunk = Telemetry::BinTable->new({ file => $file, columns => $self->columns() });
+	    $telem_chunk->process();
+	}
+
+	my @columns;
+	if ( defined $self->columns() ){
+	    @columns = @{$self->columns()};
+	}
+	else{
+	    if (scalar(keys %telem) > 0){
+		@columns = keys %telem;
+	    }
+	    else{
+		@columns = keys %{$telem_chunk->bintable()};
+	    }
+	}
+
+	for my $col ( @columns ){
+	    if ( not defined $telem{$col} ){
+		$telem{$col} = $telem_chunk->bintable->{$col};
+	    }
+	    else{
+		# if we have data, append it
+		if (UNIVERSAL::isa( $telem{$col}, 'PDL' )){
+		    my $time_dim = time_dim( $telem_chunk->bintable->{$col}, $telem_chunk->length() );
+		    my $temp = $telem{$col}->glue( $time_dim, $telem_chunk->bintable->{$col} );
+		    $telem{$col} = $temp;
+		}
+		else{
+		    if ( ref($telem{$col}) eq 'ARRAY' ){
+			push @{$telem{$col}}, @{$telem_chunk->bintable->{$col}};
+		    }
+		    else{
+			croak(__PACKAGE__ . " Unexpected data type when trying to append data for $col ");
+		    }
+		}
+	    }
+	}
+
+
+    }
+
+    $self->telem(\%telem);
+
+}
+
+
+
+
+sub time_dim{
+    
+    my $pdl = shift;
+    my $length = shift;
+    
+    my @dimlist;
+    for my $dim (0 ... $pdl->getndims()-1){
+	push @dimlist, $pdl->getdim($dim);
+    }
+
+    my $dimpdl = pdl( @dimlist );
+    my $dim_index = which( $dimpdl == $length );
+    my @dim_index_list = $dim_index->list;
+    my $time_dim = $dim_index_list[-1];
+
+    return $time_dim;
+}
+
+1;
+
+
+
+package Telemetry::Interval::ACA0;
+
+use strict; 
+use warnings;
+use Carp;
+use Data::Dumper;
+use base 'Telemetry::Interval';
+
+our @ISA =  qw( Telemetry::Interval );
+
+sub new{
+    my $class = shift;
+    my $arg_in = shift;
+
+    $arg_in->{telemtype} = 'aca0';
+
+    my $self = Telemetry::Interval->new($arg_in);
+    bless $self, $class;
+    return $self;
+
+}
+
+
+
+1;
+
+
+
--- aca_health.pl
+++ aca_health.pl
@@ -3,7 +3,7 @@
 
 use warnings;
 use strict;
-use CFITSIO::Simple;
+
 
 
 my @columns = ('quality', 'time', 'imgraw');
@@ -13,16 +13,19 @@ my @columns = ('quality', 'time', 'imgraw');
 my @eightbyeightcolumns = @columns;
 
 my @ccdmcols = ('time', 'quality', 'cobsrqid' );
+#my @ccdmcols = ('time', 'quality' );
 
 #
 
-
+require "/proj/gads6/jeanproj/perigee_health_plots/Telemetry.pm";
 use Carp;
 use PDL;
 use Data::Dumper;
 
-#my @ccdm_file_list = glob("ccdm*gz");
-#my %ccdm = combine_telem(\@ccdm_file_list, \@ccdmcols);
+my @ccdm_file_list = glob("ccdm*gz");
+my $ccdm = Telemetry::Interval->new({ file_list => \@ccdm_file_list, columns => \@ccdmcols});
+$ccdm->combine_telem();
+
 
 my @aca0_0_list = glob("aca*_0_*gz");
 my @aca0_1_list = glob("aca*_1_*gz");
@@ -34,332 +37,169 @@ my @aca0_6_list = glob("aca*_6_*gz");
 my @aca0_7_list = glob("aca*_7_*gz");
 
 
-#my %aca0_0 = combine_telem(\@aca0_0_list, \@eightbyeightcolumns );
-#my %aca0_1 = combine_telem(\@aca0_1_list, \@eightbyeightcolumns );
-my %aca0_2 = combine_telem(\@aca0_2_list, { type => 'aca8x8'} );
-#my %aca0_3 = combine_telem(\@aca0_3_list, \@eightbyeightcolumns );
-#my %aca0_4 = combine_telem(\@aca0_4_list, \@eightbyeightcolumns );
-#my %aca0_5 = combine_telem(\@aca0_5_list, \@eightbyeightcolumns );
-#my %aca0_6 = combine_telem(\@aca0_6_list, \@eightbyeightcolumns );
-#my %aca0_7 = combine_telem(\@aca0_7_list, \@eightbyeightcolumns );
 
-my $tstart = 233554382.20;
-my $tstop = 233554390.40;
+my $aca0_0 = Telemetry::Interval::ACA0->new({ file_list => \@aca0_0_list } );
+$aca0_0->combine_telem();
+my $aca0_1 = Telemetry::Interval::ACA0->new({ file_list => \@aca0_1_list } );
+$aca0_1->combine_telem();
+my $aca0_2 = Telemetry::Interval::ACA0->new({ file_list => \@aca0_2_list } );
+$aca0_2->combine_telem();
+my $aca0_6 = Telemetry::Interval::ACA0->new({ file_list => \@aca0_6_list } );
+$aca0_6->combine_telem();
+my $aca0_7 = Telemetry::Interval::ACA0->new({ file_list => \@aca0_7_list } );
+$aca0_7->combine_telem();
 
-my $match = which( ($aca0_2{time} >= ($tstart - 60)) & ($aca0_2{time} <= ($tstop + 20)));
-my @match_array = list $match;
+
+print "time ", $aca0_7->telem()->{time}->nelem(), "\n";
+print "hdr3 ", $aca0_7->telem()->{hd3tlm76}->nelem(), "\n";
+
+my $maxtimepdl = pdl( $aca0_0->telem()->{time}->max, 
+		      $aca0_1->telem()->{time}->max, 
+		      $aca0_2->telem()->{time}->max,
+		      $aca0_6->telem()->{time}->max,
+		      $aca0_7->telem()->{time}->max);
+my $maxtime =  $maxtimepdl->min ;
+my $mintimepdl = pdl( $aca0_0->telem()->{time}->min, 
+		      $aca0_1->telem()->{time}->min, 
+		      $aca0_2->telem()->{time}->min,
+		      $aca0_6->telem()->{time}->min,
+		      $aca0_7->telem()->{time}->min);
+
+my $mintime =  $mintimepdl->max ;
+
+my $time_interval = 20;
+my $min_samples = 4;
+my $n_intervals = ($maxtime - $mintime)/($time_interval);
+print "$n_intervals \n";
+my $obsid = 0;
+my $obsid_cnt = -1;
+
+my @result;
 
 use PDL::NiceSlice;
 
-for my $index (@match_array){
-    my $img = $aca0_2{imgraw}->(0:7,0:7,$index);
-    print $img;
-}
+for my $i ( 0 ... floor($n_intervals) ){
+#for my $i ( 0 ... 10 ){
+    my $range_start = $mintime + ($i * $time_interval);
+    my $range_end = $range_start + $time_interval;
+    my $ok_slot0 = which( ( $aca0_0->telem()->{time} >= $range_start )
+			  & ( $aca0_0->telem()->{time} < $range_end )
+			  & ( $aca0_0->telem()->{imgdim}  == 8) );
+    my $ok_slot1 = which( ( $aca0_1->telem()->{time} >= $range_start )
+			  & ( $aca0_1->telem()->{time} < $range_end )
+			  & ( $aca0_1->telem()->{imgdim} == 8 ) );
+    my $ok_slot2 = which( ( $aca0_2->telem()->{time} >= $range_start )
+			  & ( $aca0_2->telem()->{time} < $range_end )
+			  & ( $aca0_2->telem()->{imgdim} == 8 ) );
+    my $ok_slot6 = which( ( $aca0_6->telem()->{time} >= $range_start )
+			  & ( $aca0_6->telem()->{time} < $range_end )
+			  & ( $aca0_6->telem()->{imgdim} == 8 ) );
+    my $ok_slot7 = which( ( $aca0_7->telem()->{time} >= $range_start )
+			  & ( $aca0_7->telem()->{time} < $range_end )
+			  & ( $aca0_7->telem()->{imgdim} == 8 ) );
 
-my $obsid = 0;
-my $obsid_count = -1;
-
-for my $slot ( 0 ... 7 ){
-#    print "slot is $slot \n";
-
-
-}
-
-
-
-package Telemetry::Interval;
-
-use strict;
-use warnings;
-use Carp;
-
-use Class::MakeMethods::Standard::Hash (
-					scalar => [ qw(
-						       file_list
-						       desired_cols
-						       type
-						       telem
-						       )
-						    ],
-					);
-						       
-
-sub new{
-
-    my $self = {};
-    bless $self, $class;
-    my $arg_in = shift;
-
-    if (defined $arg_in->{file_list}){
-	$self->file_list($arg_in->{file_list});
-    }
-    if (defined $arg_in->{type}){
-	$self->type($arg_in->{type});
-    }
-    if (defined $arg_in->{desired_cols}){
-	$self->desired_cols($arg_in->{desired_cols});
-    }
-
-    return $self;
-}
-
-
-
-sub combine_telem{
-# assumes that longest dim is time-dependent
+#    print "$i \t", $ok_slot7, "\n";
     
-    my $self = shift;
 
-    unless ( defined $self->file_list()){
-	croak(__PACKAGE__ . "No telemetry files specified.");
-    }	     
-
-    unless ( ( defined $self->type() ) or ( defined $self->desired_cols() ) ){
-	croak(__PACKAGE__ . "Must specify type of telemetry or list of desired columns.");
-    }
-
-    my $file_list = $self->file_list();
-
-    my %template_hash;
-    my %telem;
-    
-    if (defined $self->type()){
-	my $type = $self->type();
-	if ( -e "${type}.yaml" ){
-	    %template_hash = YAML::LoadFile("${type}.yaml");
-	}
-    }
-
-    if (defined $self->desired_cols()){
-
-	my $desired_cols = $self->desired_cols();
-
-	my $template_file;
-
-	for my $file (@{$file_list}){
-
-	    my %fits = fits_read_bintbl($file);
-
-	    if ( has_all_cols( \%fits, $desired_cols) ){
-		$template_file = $file;
-		last;
-	    }
-	}
-	if (not defined $template_file){
-	    croak(__PACKAGE__ . "could not find matching columns in any telem files");
-	}
-
-	%template_hash = fits_read_bintbl( $template_file, @{$desired_cols} );
-    }
-
-
-    for my $file (@{$file_list}){
-
-	my %temp_fits = fits_read_bintbl( $file );
-
-	if (not defined $temp_fits{time}){
-	    croak(__PACKAGE__ . "$file is not telemetry, has no 'time' field");
-	}
+    if ( ( $ok_slot0->nelem >= $min_samples )
+	 and ( $ok_slot1->nelem >= $min_samples )
+	 and ( $ok_slot2->nelem >= $min_samples )
+	 and ( $ok_slot6->nelem >= $min_samples )
+	 and ( $ok_slot7->nelem >= $min_samples )){
+	my $time = medover( $aca0_7->telem()->{time}->($ok_slot7));
 	
-	for my $col (@{$desired_cols}){
-	    
-	    if ( defined $temp_fits{$col} ){
-
-		
-		if ( match_dim( $template_hash{$col}, $temp_fits{$col} )){
-		    # if this is our first shot, just do the assignment
-		    
-		    if ( not defined $telem{$col}){
-			$telem{$col} = $temp_fits{$col};
-			$telem{"${col}_valid"} = ones( $temp_fits{$col}->getdim(longest_dim($temp_fits{$col})) );
-		    }
-		    else{
-			# else, if we have real data, append it
-			if ( UNIVERSAL::isa($template_hash{$col}, 'PDL' )){
-			    my $temp = $telem{$col}->glue( longest_dim($temp_fits{$col}), $temp_fits{$col});
-			    $telem{$col} = $temp;			
-			    my $temp_valid = $telem{"${col}_valid"}->append( ones( $temp_fits{$col}->getdim( longest_dim($temp_fits{$col}))));
-			    $telem{"${col}_valid"} = $temp_valid;
-			    
-			}
-			else{
-			    if ( ref($template_hash{$col}) eq 'ARRAY'){
-				my $temp_valid = $telem{"${col}_valid"}->append( ones( scalar(@{$template_hash{$col}})));
-				$telem{"${col}_valid"} = $temp_valid;
-				push @{$telem{$col}}, @{$temp_fits{$col}};
-			    }
-			}
-		    }
-		}
-
-		else{
-		    # for the special case of aca imgraw data
-		    if ($col eq 'imgraw'){
-			%temp_fits = $self->make_imgraw_data_fit({ template => \%template_hash, tempdata => \%temp_fits });
-		    }
-		    
-		    print "inventing data for $file for $col \n";
-		    $self->make_up_data({ column => $col, template => \%template_hash, tempdata => \%temp_fits } );		    
-		    
-		}
-		
-	    }
-	    # if this file doesn't have any data, append zeros for the interval
-	    else{
-		print "inventing data for $file for $col \n";
-		$self->make_up_data({ column => $col, template => \%template_hash, tempdata => \%temp_fits });
-	    }
+	my $dac = medover( 256*( $aca0_7->telem()->{hd3tlm76}->($ok_slot7 )) 
+			   + ( $aca0_7->telem()->{hd3tlm77}->($ok_slot7)) 
+			   );
+	my $h066 = medover( $256( $aca0_0->telem()->{hd3tlm66}->($ok_slot0))
+			    + ( $aca0_0->telem()->{hd3tlm67}->($ok_slot0))
+			    );
+	my $h072 = medover( $256( $aca0_0->telem()->{hd3tlm72}->($ok_slot0))
+			    + ( $aca0_0->telem()->{hd3tlm73}->($ok_slot0))
+			    );
+	my $h074 = medover( $256( $aca0_0->telem()->{hd3tlm74}->($ok_slot0))
+			    + ( $aca0_0->telem()->{hd3tlm75}->($ok_slot0))
+			    );
+	my $h174 = medover( $256( $aca0_1->telem()->{hd3tlm74}->($ok_slot1))
+			    + ( $aca0_1->telem()->{hd3tlm75}->($ok_slot1))
+			    );
+	my $h176 = medover( $256( $aca0_1->telem()->{hd3tlm76}->($ok_slot1))
+			    + ( $aca0_1->telem()->{hd3tlm77}->($ok_slot1))
+			    );
+	my $h262 = medover( $256( $aca0_2->telem()->{hd3tlm62}->($ok_slot2))
+			    + ( $aca0_2->telem()->{hd3tlm63}->($ok_slot2))
+			    );
+	my $h264 = medover( $256( $aca0_2->telem()->{hd3tlm64}->($ok_slot2))
+			    + ( $aca0_2->telem()->{hd3tlm65}->($ok_slot2))
+			    );
+	my $h266 = medover( $256( $aca0_2->telem()->{hd3tlm66}->($ok_slot2))
+			    + ( $aca0_2->telem()->{hd3tlm67}->($ok_slot2))
+			    );
 	
-	}
-    }
+	my $h272 = medover( $256( $aca0_2->telem()->{hd3tlm72}->($ok_slot2))
+			    + ( $aca0_2->telem()->{hd3tlm73}->($ok_slot2))
+			    );
+	my $h274 = medover( $256( $aca0_2->telem()->{hd3tlm74}->($ok_slot2))
+			    + ( $aca0_2->telem()->{hd3tlm75}->($ok_slot2))
+			    );
+	my $h276 = medover( $256( $aca0_2->telem()->{hd3tlm76}->($ok_slot2))
+			    + ( $aca0_2->telem()->{hd3tlm77}->($ok_slot2))
+			    );
+	
+	my $aca_temp = medover( (1/256.)*( $aca0_7->telem()->{hd3tlm73}->($ok_slot7)) 
+				+ ( $aca0_7->telem()->{hd3tlm72}->($ok_slot7)) 
+				);
+	
+	my $ccd_temp = medover( ( 256*( $aca0_6->telem()->{hd3tlm76}->($ok_slot6 )) 
+				  + ( $aca0_6->telem()->{hd3tlm77}->($ok_slot6) ) 
+				  - 65536
+				  )
+				/ 100.
+				)
+	    ;
 
-    $self->telem(\%telem);
-    return \%telem;
+
+	print $time, " ", $dac, " ",  $ccd_temp, " ", $aca_temp, "\n";
+
+	
+#	print $aca0_7->telem()->{time}->nelem, "\n";
+#	print $aca0_7->telem()->{hd3tlm76}->nelem, "\n";
+#	print $aca0_7->telem()->{hd3tlm77}->nelem, "\n";
+#
+#	my %curr_result;
+
+    }	
 
 }
 
-sub has_all_cols{
-    my $fits = shift;
-    my $desired_cols = shift;
+print Dumper @result;
+#
+#
+#
 
-    my @keys = keys %{$fits};
-    my $match = 1;
-    for my $col (@{$desired_cols}){
-	$col =~ tr/A-Z/a-z/;
-	if (not defined $fits->{$col}){
-	    $match = 0;
-	    last;
-	}
-    }
+#my $tstart = 233554382.20;
+#my $tstop = 233554390.40;
+#
+#my $match = which( ($aca0_2{time} >= ($tstart - 60)) & ($aca0_2{time} <= ($tstop + 20)));
+#my @match_array = list $match;
+#
+#use PDL::NiceSlice;
+#
+#for my $index (@match_array){
+#    my $img = $aca0_2{imgraw}->(0:7,0:7,$index);
+#    print $img;
+#}
+#
+#my $obsid = 0;
+#my $obsid_count = -1;
+#
+#for my $slot ( 0 ... 7 ){
+##    print "slot is $slot \n";
+#
+#
+#}
+#
 
-    return $match;
-}
-
-
-
-sub make_imgraw_data_fit{
-
-    my $self = shift;
-
-    my $arg_in = shift;
-    my $col = $arg_in->{column};
-    my $template = $arg_in->{template};
-    my $tempdata = $arg_in->{tempdata};
-
-    my @imgraw_dim = $template->{$col}->dims;
-    my $longest_dim = longest_dim($template->{$col});
-    $dimarray[$longest_dim] = $tempdata->{'time'}->getdim(0);
-
-    my $temp_zero = zeroes( @dimarray );
-    if ( defined $telem{$col} ){
-	my $temp = $telem{$col}->glue( $longest_dim, $temp_zero);
-	$telem{$col} = $temp;
-	my $temp_valid = $telem{"${col}_valid"}->append( zeroes( $tempdata->{'time'}->getdim(0)));
-	$telem{"${col}_valid"} = $temp_valid;
-    }
-    else{
-	$telem{$col} = $temp_zero;
-	$telem{"${col}_valid"} = zeroes( $tempdata->{'time'}->getdim(0) );
-    }
-
-    if ( not defined $telem{$col}){
-	$telem{$col} = $temp_fits{$col};
-	$telem{"${col}_valid"} = ones( $temp_fits{$col}->getdim(longest_dim($temp_fits{$col})) );
-    }
-    else{
-	# else, if we have real data, append it
-	if (UNIVERSAL::isa($template_hash{$col}, 'PDL')){
-	    my $temp = $telem{$col}->glue( longest_dim($temp_fits{$col}), $temp_fits{$col});
-	    $telem{$col} = $temp;			
-	    my $temp_valid = $telem{"${col}_valid"}->append( ones( $temp_fits{$col}->getdim( longest_dim($temp_fits{$col}))));
-	    $telem{"${col}_valid"} = $temp_valid;
-	    
-	}
-	else{
-	    if ( ref($template_hash{$col}) eq 'ARRAY'){
-		my $temp_valid = $telem{"${col}_valid"}->append( ones( scalar(@{$template_hash{$col}})));
-		$telem{"${col}_valid"} = $temp_valid;
-		push @{$telem{$col}}, @{$temp_fits{$col}};
-	    }
-	}
-    }
-    }
-}
-
-sub make_up_data{
-    my $self = shift;
-    my $arg_in = shift;
-    my $col = $arg_in->{column};
-    my $template_href = $arg_in->{template};
-    my $temp_fits_href = $arg_in->{tempdata};
-    my $telem_href = $self->telem();
-    my @dimarray = $template_href->{$col}->dims;
-    my $longest_dim = longest_dim($template_href->{$col});
-    $dimarray[$longest_dim] = $temp_fits_href->{'time'}->getdim(0);
-    my $temp_zero = zeroes( @dimarray );
-    if ( defined $telem_href->{$col} ){
-	my $temp = $telem_href->{$col}->glue( $longest_dim, $temp_zero);
-	$telem_href->{$col} = $temp;
-	my $temp_valid = $telem_href->{"${col}_valid"}->append( zeroes( $temp_fits_href->{'time'}->getdim(0)));
-	$telem_href->{"${col}_valid"} = $temp_valid;
-    }
-    else{
-	$telem_href->{$col} = $temp_zero;
-	$telem_href->{"${col}_valid"} = zeroes( $temp_fits_href->{'time'}->getdim(0) );
-    }
-}
-
-
-sub match_dim{
-
-    my $template = shift;
-    my $test = shift;
-
-    if ($template->ndims != $test->ndims){
-	return 0;
-    }
-
-    my $longest_dim = longest_dim( $template );
-    my @template_dims = $template->dims;
-    my @test_dims = $test->dims;
-    $test_dims[$longest_dim] = $template->getdim($longest_dim);
-    for my $i ( 0 .. ($template->ndims) - 1 ){
-	if ( $template_dims[$i] != $test_dims[$i]){
-	    return 0;
-	}
-    }
-    return 1;
-}
-
-
-sub longest_dim{
-    
-    my $pdl = shift;
-    my @dimlist;
-    for my $dim (0 ... $pdl->getndims()-1){
-	push @dimlist, $pdl->getdim($dim);
-    }
-    my $dimpdl = pdl( @dimlist );
-    my $dim_index = which( $dimpdl == $dimpdl->max());
-    my $longest_dim = $dim_index->at(0);
-
-    return $longest_dim;
-}
-
-
-
-1;
-
-package Telemetry::Interval::ACA0;
-
-use strict; 
-use warnings;
-use Carp;
-
-use base 'Telemetry::Interval';
-
-our ISA =  qw( Telemetry::Interval );
-1;
 
 
 
